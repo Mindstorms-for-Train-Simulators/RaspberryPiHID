9038f79e0504867b8f10286dc694b0cf70edd0ef
GrumpyMusician <100326980+GrumpyMusician@users.noreply.github.com>
2025-07-25 16:15:04 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,6 @@
-echo -ne '\x00\x00' > /dev/hidg0
+#!/bin/bash
+# Throttle: 128, Modifier: none, Key: 'w' (0x1A)
+
+echo -ne '\x80\x00\x1A' > /dev/hidg0
+sleep 0.1
+echo -ne '\x80\x00\x00' > /dev/hidg0  # release
